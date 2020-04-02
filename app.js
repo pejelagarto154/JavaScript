c9558c831e2038acbfd49f164249804dbef0a33a
matias <lagarto154@gmail.com>
2020-04-02 19:22:23 -0400

    avanzando 3

--- app.js
+++ app.js
@@ -1,1 +1,4 @@
-document.getElementById('app').innerHTML='hola mundo';
+let nombre=prompt('Cual es tu nombre');
+let edad=prompt('Cual es tu edad');
+
+document.getElementById('app').innerHTML=`Bienvenido ${nombre} de ${edad} años`;
