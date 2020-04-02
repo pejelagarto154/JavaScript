2e6df4adeb4cd40ddb00eb977bba14647d70abe7
matias <lagarto154@gmail.com>
2020-04-02 19:51:59 -0400

    avanzando 5

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
-let nombre=prompt('Cual es tu nombre');
-let edad=prompt('Cual es tu edad');
+// let nombre=prompt('Cual es tu nombre');
+// let edad=prompt('Cual es tu edad');
 
-confirm('deseas agregar tu nombre y edad');
+// confirm('deseas agregar tu nombre y edad');
 console.log('todo bien');
+console.table([1,2,3]);
+console.error('algo salio mal');
+console.warn('ufff');
+console.time('uno');
+console.error('algo salio mal');
+console.error('algo salio mal');
+console.timeEnd('uno');
 document.getElementById('app').innerHTML=`Bienvenido ${nombre} de ${edad} años`;
