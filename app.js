60a7ffb7c122cf84ff0144ab81f2c8670ae2d3cb
matias <lagarto154@gmail.com>
2020-04-02 19:32:05 -0400

    avanzando 4

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 let nombre=prompt('Cual es tu nombre');
 let edad=prompt('Cual es tu edad');
 
+confirm('deseas agregar tu nombre y edad');
+console.log('todo bien');
 document.getElementById('app').innerHTML=`Bienvenido ${nombre} de ${edad} años`;
